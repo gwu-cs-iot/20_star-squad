9fdf044bfa5d86a50e675b5f1de6ab356e60dcd2
lrshpak <lrshpak@gwu.edu>
2020-03-07 18:56:38 -0500

    more messing with the api

--- testFiles/airPollutionTest.py
+++ testFiles/airPollutionTest.py
@@ -4,10 +4,26 @@ import requests
 
 url = "http://api.airvisual.com/v2/countries?key=d3d2e390-b9b3-4482-a2aa-52278709287f"
 
+#make the call with the url 
 payload = {}
 files = {}
 headers= {}
-
 response = requests.request("GET", url, headers=headers, data = payload, files = files)
 
-print(response.text.encode('utf8'))
+
+data = response.json()
+
+#prints out all the countries
+for p in data['data']:
+	print("Country: " + p['country'] + "\n");
+
+#make the call with the url to query all valid cities in NH
+
+url2 = "http://api.airvisual.com/v2/cities?state=New Hampshire&country=USA&key=d3d2e390-b9b3-4482-a2aa-52278709287f"
+payload2 = {}
+files2 = {}
+headers2= {}
+response2 = requests.request("GET", url2, headers=headers, data = payload, files = files)
+data2 = response2.json()
+for p in data2['data']:
+	print("City: " + p['city'] + "\n");
